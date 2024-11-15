5152d353bca5bf94e6e829b00f51c5f4bdf84b11
Mark Langston <160653921+Mark-Langston@users.noreply.github.com>
2024-11-15 07:55:17 -0800

    Update Automobile.cpp

--- Automobile.cpp
+++ Automobile.cpp
@@ -1,12 +1,1 @@
-#ifndef VEHICLE_H
-#define VEHICLE_H
-
-#include <string>
-
-class Vehicle {
-public:
-    virtual std::string getType() const = 0;
-    virtual ~Vehicle() {}
-};
-
-#endif // VEHICLE_H
+#include "Automobile.h"

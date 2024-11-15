97637efdab2edc314b061c90c4296b7f3ae64414
Mark Langston <160653921+Mark-Langston@users.noreply.github.com>
2024-11-15 07:57:34 -0800

    Update TrafficLightSystem.cpp

--- TrafficLightSystem.cpp
+++ TrafficLightSystem.cpp
@@ -1,21 +1,32 @@
 #include "TrafficLightSystem.h"
 #include <iostream>
 
+TrafficLightSystem::TrafficLightSystem(int maxQueueLength) {
+    for (int i = 0; i < 4; ++i) {
+        queues.push_back(QueueManager(maxQueueLength));
+    }
+}
+
 void TrafficLightSystem::handleVehicle(Vehicle* vehicle, int road) {
-    // Adjust road number to match internal array indexing (1-4)
     int adjustedRoad = road - 1;
 
     if (vehicle->getType() == "Emergency Vehicle") {
-        std::cout << "Emergency vehicle detected! Turning all lights red.\n";
+        std::cout << "\nEmergency vehicle detected! Turning all lights red." << std::endl;
         eventFlag.setEmergencyDetected(true);
-    } else {
-        std::cout << "Adding " << vehicle->getType() << " to road " << road << ".\n";
-        queues[adjustedRoad].addVehicleToQueue(vehicle);
+        return;
+    }
 
-        // Check if the queue is full
-        if (queues[adjustedRoad].isQueueFull()) {
-            std::cout << "Queue is full on road " << road << "! Turning the light green to clear traffic.\n";
-            queues[adjustedRoad].clearQueue();
-        }
+    std::cout << "\nAdding " << vehicle->getType() << " to road " << road << "." << std::endl;
+    queues[adjustedRoad].addVehicleToQueue(vehicle);
+
+    int currentQueueLength = queues[adjustedRoad].getQueueLength();
+
+    std::cout << "Automobile added to the queue." << std::endl;
+    std::cout << "Total cars on this road: " << currentQueueLength << std::endl;
+    std::cout << "Remaining cars needed to turn the light green: " << (10 - currentQueueLength) << std::endl;
+
+    if (queues[adjustedRoad].isQueueFull()) {
+        std::cout << "\nQueue is full on road " << road << "! Turning the light green to clear traffic." << std::endl;
+        queues[adjustedRoad].clearQueue();
     }
 }

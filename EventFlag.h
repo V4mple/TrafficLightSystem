e077313758f57ba27775eae580813287d6fd1f61
Mark Langston <160653921+Mark-Langston@users.noreply.github.com>
2024-11-19 09:58:45 -0800

    Update EventFlag.h

--- EventFlag.h
+++ EventFlag.h
@@ -1,17 +1,21 @@
 #ifndef EVENTFLAG_H
 #define EVENTFLAG_H
 
+// Class to handle event flags for the Traffic Light System
 class EventFlag {
 private:
-    bool emergencyDetected;
+    bool emergencyDetected; // Flag to indicate if an emergency is detected
 
 public:
+    // Constructor initializes the emergency flag to false
     EventFlag() : emergencyDetected(false) {}
 
+    // Sets the emergency flag to the specified status
     void setEmergencyDetected(bool status) {
         emergencyDetected = status;
     }
 
+    // Returns the current status of the emergency flag
     bool isEmergencyDetected() const {
         return emergencyDetected;
     }

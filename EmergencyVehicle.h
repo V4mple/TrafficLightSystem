c54b515260c02d9bbd2839417a83d25d5fa160b3
Mark Langston <160653921+Mark-Langston@users.noreply.github.com>
2024-11-19 09:57:26 -0800

    Update EmergencyVehicle.h

--- EmergencyVehicle.h
+++ EmergencyVehicle.h
@@ -1,11 +1,13 @@
 #ifndef EMERGENCY_VEHICLE_H
 #define EMERGENCY_VEHICLE_H
 
-#include "Vehicle.h"
+#include "Vehicle.h" // Base class for all vehicle types
 #include <string>
 
+// Class representing an emergency vehicle, inheriting from the Vehicle base class
 class EmergencyVehicle : public Vehicle {
 public:
+    // Returns the type of this vehicle as a string
     std::string getType() const override { return "Emergency Vehicle"; }
 };
 

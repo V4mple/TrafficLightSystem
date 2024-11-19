0457ecbfbe9c1e3049f977b84c92e466ea73ddaf
Mark Langston <160653921+Mark-Langston@users.noreply.github.com>
2024-11-19 09:56:13 -0800

    Update Automobile.h

--- Automobile.h
+++ Automobile.h
@@ -1,11 +1,13 @@
 #ifndef AUTOMOBILE_H
 #define AUTOMOBILE_H
 
-#include "Vehicle.h"
+#include "Vehicle.h" // Base class for all vehicle types
 #include <string>
 
+// Class representing an automobile, inheriting from the Vehicle base class
 class Automobile : public Vehicle {
 public:
+    // Returns the type of this vehicle as a string
     std::string getType() const override { return "Automobile"; }
 };
 

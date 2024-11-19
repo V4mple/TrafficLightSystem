22f608edb4ae94b94ea359ff92303ba476341376
Mark Langston <160653921+Mark-Langston@users.noreply.github.com>
2024-11-19 09:59:11 -0800

    Update QueueManager.cpp

--- QueueManager.cpp
+++ QueueManager.cpp
@@ -1,19 +1,23 @@
 #include "QueueManager.h"
 
+// Adds a vehicle to the queue
 void QueueManager::addVehicleToQueue(Vehicle* vehicle) {
-    queue.push(vehicle);
+    queue.push(vehicle); // Push the vehicle to the back of the queue
 }
 
+// Returns the current length of the queue
 int QueueManager::getQueueLength() const {
-    return queue.size();
+    return queue.size(); // Return the number of vehicles in the queue
 }
 
+// Checks if the queue has reached its maximum allowed length
 bool QueueManager::isQueueFull() const {
-    return queue.size() >= maxQueueLength;
+    return queue.size() >= maxQueueLength; // Compare queue size to the max length
 }
 
+// Clears all vehicles from the queue
 void QueueManager::clearQueue() {
     while (!queue.empty()) {
-        queue.pop();
+        queue.pop(); // Remove vehicles from the front until the queue is empty
     }
 }
